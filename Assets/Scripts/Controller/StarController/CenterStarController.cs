13527c0b805a1168509b7ada8cafbc4a3708aea8
Asakura <106337231+CrazyHuman00@users.noreply.github.com>
2024-06-09 19:29:43 +0900

    add: 一部星の軌道を追加

---
+++ Assets/Scripts/Controller/StarController/CenterStarController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 星の移動を制御するクラス
+/// <summary>
+public class CenterStarController : MonoBehaviour
+{
+    [SerializeField] private float starSpeed;
+    [SerializeField] private float rotSpeed;
+    [SerializeField] private float startStarPositionY;
+    [SerializeField] private float endStarPositionY;
+    [SerializeField] private float spanDelay;
+    private float starPositionX;
+    private float screenLeftBottom; // modelに移動
+    private float screenRightTop; // modelに移動
+    private bool isSpanning = false;
+
+    void Start()
+    {
+        screenLeftBottom = Camera.main.ScreenToWorldPoint(Vector2.zero).x + 5.0f;
+        screenRightTop = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)).x - 5.0f;
+
+        // 初期のx座標をランダムに設定
+        starPositionX = Random.Range(screenLeftBottom, screenRightTop);
+        transform.position = new Vector2(starPositionX, startStarPositionY);
+    }
+
+    void Update()
+    {
+        if (!isSpanning)
+        {
+            transform.Translate(0, starSpeed * Time.deltaTime, 0, Space.World);
+            transform.Rotate(0, 0, rotSpeed * Time.deltaTime);
+
+            if (transform.position.y > endStarPositionY)
+            {
+                StartCoroutine(SpanStar());
+            }
+        }
+    }
+
+    private IEnumerator SpanStar()
+    {
+        isSpanning = true;
+        yield return new WaitForSeconds(spanDelay);
+
+        starPositionX = Random.Range(screenLeftBottom, screenRightTop);
+        transform.position = new Vector2(starPositionX, startStarPositionY);
+        starSpeed += 0.1f * Random.value;
+
+        isSpanning = false;
+    }
+
+}
---
+++ Assets/Scripts/Controller/StarController/LeftSideStarController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftSideStarController : MonoBehaviour
+{
+    [SerializeField] private float starSpeed;
+    [SerializeField] private float rotSpeed;
+    [SerializeField] private float startStarPositionX;
+    [SerializeField] private float endStarPositionX;
+    [SerializeField] private float endStarPositionY;
+    [SerializeField] private float spanDelay;
+    private float starPositionY;
+    private float screenLeftBottom; // modelに移動
+    private float screenRightTop; // modelに移動
+    private bool isSpanning = false;
+    private float starSpeedY;
+
+
+    void Start()
+    {
+        screenLeftBottom = Camera.main.ScreenToWorldPoint(Vector2.zero).y;
+
+        // 初期のy座標をランダムに設定
+        starPositionY = Random.Range(screenLeftBottom, 0);
+        transform.position = new Vector2(startStarPositionX, starPositionY);
+    }
+
+    void Update()
+    {
+        if (!isSpanning)
+        {
+            transform.Translate(starSpeed * Time.deltaTime, starSpeedY * Time.deltaTime, 0, Space.World);
+            transform.Rotate(0, 0, rotSpeed * Time.deltaTime);
+            
+            if (transform.position.x > endStarPositionX || transform.position.y > endStarPositionY)
+            {
+                StartCoroutine(SpanStar());
+            }
+        }
+    }
+
+    private IEnumerator SpanStar()
+    {
+        isSpanning = true;
+        yield return new WaitForSeconds(spanDelay);
+
+        starPositionY = Random.Range(screenLeftBottom, screenRightTop);
+        transform.position = new Vector2(startStarPositionX, starPositionY);
+        starSpeed += 0.1f * Random.value;
+        starSpeedY = Random.Range(0f, 3f);
+
+        isSpanning = false;
+    }
+}
