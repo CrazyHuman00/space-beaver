fd3f6d80bd3e2effefe3316cfad55fa1b55dc952
Asakura <106337231+CrazyHuman00@users.noreply.github.com>
2024-06-09 23:43:33 +0900

    add: 星とプレイヤーの当たり判定

--- Assets/Scripts/Controller/StarController/RightStarController.cs
+++ Assets/Scripts/Controller/StarController/RightStarController.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using WaitTime;
+
+/// <summary>
+/// 右からの星の移動を制御するクラス
+/// <summary>
 public class RightStarController : MonoBehaviour
 {
     [SerializeField] private float starSpeed;
@@ -10,10 +15,11 @@ public class RightStarController : MonoBehaviour
     [SerializeField] private float endStarPositionX;
     [SerializeField] private float endStarPositionY;
     [SerializeField] private float spanDelay;
-
+    [SerializeField] private float startTime;
     private float starPositionY;
     private float screenLeftBottom; // modelに移動
     private bool isSpanning = false;
+    private bool isActive = false;
     private float starSpeedY;
 
 
@@ -24,12 +30,16 @@ public class RightStarController : MonoBehaviour
         // 初期のy座標をランダムに設定
         starPositionY = Random.Range(screenLeftBottom - 5.0f, 0);
         transform.position = new Vector2(startStarPositionX, starPositionY);
+
+        // 開始時間を決める
+        StartCoroutine(ActivateAfterDelay(startTime));
     }
 
 
     void Update()
     {
-        if (!isSpanning)
+        
+        if (isActive && !isSpanning)
         {
             transform.Translate(-starSpeed * Time.deltaTime, starSpeedY * Time.deltaTime, 0, Space.World);
             transform.Rotate(0, 0, rotSpeed * Time.deltaTime);
@@ -44,7 +54,7 @@ public class RightStarController : MonoBehaviour
     private IEnumerator SpanStar()
     {
         isSpanning = true;
-        yield return new WaitForSeconds(spanDelay);
+        yield return StartCoroutine(StarWaitTime.WaitForSecondsCoroutine(spanDelay));
 
         starPositionY = Random.Range(screenLeftBottom, 0);
         transform.position = new Vector2(startStarPositionX, starPositionY);
@@ -52,5 +62,11 @@ public class RightStarController : MonoBehaviour
         starSpeedY = Random.Range(0, 3f);
 
         isSpanning = false;
+    }
+
+    private IEnumerator ActivateAfterDelay(float delay)
+    {
+        yield return StartCoroutine(StarWaitTime.WaitForSecondsCoroutine(delay));
+        isActive = true; 
     }
 }
