5203e95845e9607afb7c8ef4d1d7188c958cc879
Asakura <106337231+CrazyHuman00@users.noreply.github.com>
2024-06-10 02:03:06 +0900

    add: 画像追加

---
+++ Assets/Scripts/InGame/Model/PlayerLifeModel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Common.Model
+{
+    public class PlayerLifeModel : MonoBehaviour
+    {
+        [SerializeField] private int playerLife = 3;
+        
+    }
+}
