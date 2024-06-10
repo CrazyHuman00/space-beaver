2bdc3bdfedb085c8af2b39c843ebdb2825f625c3
Asakura <106337231+CrazyHuman00@users.noreply.github.com>
2024-06-11 01:45:21 +0900

    炎隕石の追加

--- Assets/Scripts/InGame/Model/GamePlayTime.cs
+++ Assets/Scripts/InGame/Model/GamePlayTime.cs
@@ -1,14 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 namespace InGame.Model
 {
     public class TimeManager : MonoBehaviour
     {
+        // Debug用
+        [SerializeField] TextMeshProUGUI debugText;
+        private int a;
+        public static TimeManager Instance { get; private set; }
+        private float elapsedTime;
+        private bool isRunning = false;
+        
+
+        private void Start()
+        {
+            elapsedTime = 0.0f;
+            isRunning = true;
+        }
+
+        public float GetElapsedTime()
+        {
+            if (isRunning)
+            {
+                return elapsedTime;
+            }
+            else
+            {
+                return 0f;
+            }
+        }
+
         void Update()
         {
-            // Debug.Log ("経過時間(秒)" + Time.time);
+            elapsedTime += Time.deltaTime;
+            a = (int)elapsedTime;
+            debugText.text = a.ToString();
         }
     }
 
